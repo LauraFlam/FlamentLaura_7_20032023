f4229dd8f4264029f77ebdcb0066eab309bbb352
Laura Flament <laura.flament6@gmail.com>
2023-03-25 22:31:23 +0100

    Création de l'application react

---
+++ kasa/src/App.js
@@ -0,0 +1,34 @@
+import Home from './pages/Home/Home';
+/*import Error from './pages/Error/Error';
+import About from './pages/About/About';
+import Logements from './pages/Logements/Logements';*/
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />
+	},
+	/*{
+		path: "/logements/:id",
+		element: <Logements />
+	},
+	{
+		path: '/about',
+		element: <About />
+	},
+	{
+		path: "*",
+		element: <Error />
+	},*/
+]);
+
+function App() {
+	return (
+		<>
+			<RouterProvider router={router}/>
+		</>
+	);
+}
+
+export default App;
---
+++ kasa/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import Header from '../../components/Header/Header'
+import Banner from '../../components/Banner/Banner'
+import Footer from '../../components/Footer/Footer'
+
+export default function Home() {
+	return (
+		<div className='home'>
+			<Header />
+			<Banner />
+			<Footer />
+		</div>
+	)
+}
